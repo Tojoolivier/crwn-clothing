ba6c984075212172af5f9272bf27a97e93f65445
Tojo Olivier <tojo.niriina@gmail.com>
2021-12-22 15:37:23 +0300

    setup and add HomePage

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomePage from './components/Homepage/homepage'
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <HomePage/>
+    </div>
+  );
+}
+
+export default App;
